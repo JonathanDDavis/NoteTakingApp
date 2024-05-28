2f99ae2296b1617e6ec64d45cfbfe813702a37cd
JonathanDDavis <jonathanddavis2000@gmail.com>
2024-05-28 14:37:26 -0500

    Initial commit

---
+++ NoteTakingApp/NoteTakingApp/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NoteTakingApp
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
